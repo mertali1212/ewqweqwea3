105131dcee5c6f769c55ed89e038c1575a6318bb
mertali1212 <99125788+mertali1212@users.noreply.github.com>
2022-02-26 00:17:57 +0300

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "OTM4OTUwNzczNzg0NTk2NTEx..EVF4W1RB3T0JDTz-kHgz8wyKlqQ",
+    prefix: "vc!",
+   admins: [
+        "586858102834593812",
+        "781935014405341215",
+        "599884419163095040",
+   ],
+    debug: true,
+    countChannel: "countChannelID"
+};
---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = (client) => {
+    console.log(`${client.user.tag} Rahatsız Etme`);
+  client.user.setActivity("Leet Hotel Vice City");
+};
